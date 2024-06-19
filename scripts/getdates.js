03b334fdea952b90fea68c7fd09011d65d9e488e
John Wagner <john.wagnerm@gmail.com>
2024-06-19 00:23:03 -0500

    js changes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,6 @@
 const year = new Date().getFullYear();
-document.getElementById("currentyear").innerHTML = `&#169 ${year}`;
+const element=document.getElementById("currentyear")
+element.innerHTML = `&#169 ${year}`;
 const now = new Date(document.lastModified).toLocaleDateString();
 const time = new Date(document.lastModified).toLocaleTimeString();
 document.getElementById(
