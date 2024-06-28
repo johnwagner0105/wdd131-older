90c1acce019ae14c15b2409559db2313277d00d9
John Wagner <john.wagnerm@gmail.com>
2024-06-28 18:05:56 -0500

    Week 3 assignment

---
+++ scripts/place.js
@@ -0,0 +1,17 @@
+const year = new Date().getFullYear();
+const element = document.getElementById("currentyear");
+element.innerHTML = `&#169; ${year}`;
+
+const now = new Date(document.lastModified).toLocaleDateString();
+const time = new Date(document.lastModified).toLocaleTimeString();
+document.getElementById("lastModified").innerHTML = `Last Modified: ${now} ${time}`;
+
+const temp=9;
+const windSpeed=13;
+
+const calculateWindChill=()=>{
+    const windChill=13.12+(0.6215*temp)-(11.37*((windSpeed**0.16)))+(0.3965*temp*(windSpeed**0.16))
+    return windChill;
+}
+
+const displayWindChill=document.getElementById("wind-chill").innerHTML=calculateWindChill().toFixed(1) + " °C";
