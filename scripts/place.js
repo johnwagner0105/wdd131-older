6eb376de7220cfa0d1867e2dee23152aac24e821
John Wagner <john.wagnerm@gmail.com>
2024-06-28 19:04:01 -0500

    js changes

--- scripts/place.js
+++ scripts/place.js
@@ -1,17 +1,19 @@
-const year = new Date().getFullYear();
-const element = document.getElementById("currentyear");
-element.innerHTML = `&#169; ${year}`;
+document.addEventListener("DOMContentLoaded", () => {
+    const year = new Date().getFullYear();
+    const element = document.getElementById("currentyear");
+    element.innerHTML = `&#169; ${year}`;
 
-const now = new Date(document.lastModified).toLocaleDateString();
-const time = new Date(document.lastModified).toLocaleTimeString();
-document.getElementById("lastModified").innerHTML = `Last Modified: ${now} ${time}`;
+    const now = new Date(document.lastModified).toLocaleDateString();
+    const time = new Date(document.lastModified).toLocaleTimeString();
+    document.getElementById("lastModified").innerHTML = `Last Modified: ${now} ${time}`;
 
-const temp=9;
-const windSpeed=13;
+    const temp = 9;
+    const windSpeed = 13;
 
-const calculateWindChill=()=>{
-    const windChill=13.12+(0.6215*temp)-(11.37*((windSpeed**0.16)))+(0.3965*temp*(windSpeed**0.16))
-    return windChill;
-}
+    const calculateWindChill = () => {
+        const windChill = 13.12 + (0.6215 * temp) - (11.37 * (windSpeed ** 0.16)) + (0.3965 * temp * (windSpeed ** 0.16));
+        return windChill;
+    }
 
-const displayWindChill=document.getElementById("wind-chill").innerHTML=calculateWindChill().toFixed(1) + " °C";
+    document.getElementById("wind-chill").innerHTML = calculateWindChill().toFixed(1) + " °C";
+});
