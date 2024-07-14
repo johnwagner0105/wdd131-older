03d401ad33403d0be59596c47290edac5fe9c40f
John Wagner <john.wagnerm@gmail.com>
2024-07-14 00:47:18 -0500

    Week 5 assignment

--- scripts/form.js
+++ scripts/form.js
@@ -5,3 +5,44 @@ element.innerHTML = `&#169; ${year}`;
 const now = new Date(document.lastModified).toLocaleDateString();
 const time = new Date(document.lastModified).toLocaleTimeString();
 document.getElementById("lastModified").innerHTML = `Last Modified: ${now} ${time}`;
+
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+const showProducts=document.getElementById("productSelection");
+const displayProducts=()=>{
+    let product
+    products.forEach((element)=>{
+        product=document.createElement("option");
+        product.value=element.id;
+        product.text=element.name;
+        showProducts.appendChild(product)
+    })
+}
+
+displayProducts();
